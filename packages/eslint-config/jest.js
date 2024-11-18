079587be00fedc37d687f6fb6a091d212016c059
andre faria <fariaoandre@gmail.com>
2024-11-18 10:40:16 +0000

    chore: init proj structure prettier eslint jest cypress ts react vite

---
+++ packages/eslint-config/jest.js
@@ -0,0 +1,16 @@
+
+module.exports = {
+  env: {
+    node: true,
+  },
+  overrides: [
+    {
+      files: ['src/**/*.test.ts', 'src/**/*.test.tsx'],
+      extends: ['plugin:jest/recommended'],
+      plugins: ['jest'],
+      env: {
+        'jest/globals': true,
+      },
+    },
+  ],
+};
