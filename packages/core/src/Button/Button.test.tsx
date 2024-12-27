a80cf1224242ca5a89558b0252306cac3fd1aaec
andre faria <fariaoandre@gmail.com>
2024-12-27 14:33:43 +0000

    feat: button component with tests

--- packages/core/src/Button/Button.test.tsx
+++ packages/core/src/Button/Button.test.tsx
@@ -1,12 +1,31 @@
+import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+import { Button } from './Button'
 
-// import '@testing-library/jest-dom'
-// import { render } from '@testing-library/react'
-// import { Button } from "./Button";
+describe('Button', () => {
+  test('renders with correct text', () => {
+    render(<Button>Click Me</Button>)
+    const button = screen.getByRole('button', { name: /click me/i })
+    expect(button).toBeInTheDocument()
+  })
 
-// describe('Button', () => {
-//   it('renders correctly', () => {
-//     const { getByText } = render(<Button>Click me</Button>);
-//     expect(getByText('Click me')).toBeInTheDocument();
-//   });
+  test('button clicked', () => {
+    const fn = jest.fn()
+    render(<Button onClick={fn}>"Click"</Button>)
+    const button = screen.getByRole('button')
+    button.click()
+    expect(fn).toHaveBeenCalledTimes(1)
+  })
 
-// });
+  test('button hovered', async () => {
+    render(<Button>Click</Button>)
+    const button = screen.getByRole('button')
+    await userEvent.hover(button)
+    expect(button).toHaveAttribute('data-hover')
+  })
+
+  test('default button type is button', () => {
+    render(<Button>Click</Button>)
+    expect(screen.getByRole('button')).toHaveAttribute('type', 'button')
+  })
+})
--- packages/core/src/Button/Button.tsx
+++ packages/core/src/Button/Button.tsx
@@ -1,11 +1,110 @@
-import { ElementType } from 'react'
-import { ButtonProps } from './Button.types'
+import { ElementType, MouseEvent, useCallback, useMemo, useState } from 'react'
+import { ButtonProps, ButtonState } from './Button.types'
 
 let DEFAULT_BUTTON_TAG = 'button' as const
+
+const useHover = (isDisabled: boolean) => {
+  const [isHovered, setIsHovered] = useState(false)
+
+  const handeMouseEnter = (ev: MouseEvent) => {
+    console.log(ev)
+    if (!isDisabled) {
+      setIsHovered(true)
+    }
+  }
+  const handeMouseLeave = (ev: MouseEvent) => {
+    console.log(ev)
+    if (!isDisabled) {
+      setIsHovered(false)
+    }
+  }
+
+  return {
+    isHover: isHovered,
+    hoverProps: {
+      onMouseEnter: handeMouseEnter,
+      onMouseLeave: handeMouseLeave,
+    },
+  }
+}
+
+const usePress = (disabled: boolean) => {
+  const [isPressed, setIsPressed] = useState(false)
+
+  const handleDown = () => !disabled && setIsPressed(true)
+  const handleUp = () => !disabled && setIsPressed(false)
+
+  return {
+    isPressed,
+    pressProps: {
+      onMouseDown: handleDown,
+      onMouseUp: handleUp,
+    },
+  }
+}
+
+const useFocus = () => {
+  const [isFocused, setIsFocused] = useState(false)
+
+  const handleFocus = useCallback(() => {
+    setIsFocused(true)
+  }, [])
+
+  const handleBlur = useCallback(() => {
+    setIsFocused(false)
+  }, [])
+
+  return {
+    isFocused,
+    focusProps: {
+      onFocus: () => {
+        handleFocus()
+      },
+      onBlur: () => {
+        handleBlur()
+      },
+    },
+  }
+}
 
 export const Button = <T extends ElementType = typeof DEFAULT_BUTTON_TAG>({
   as,
   ...props
 }: ButtonProps<T>) => {
-  return <button {...props}></button>
+  const { disabled = false, autofocus, type, children } = props
+  const { isFocused, focusProps } = useFocus()
+  const { isPressed, pressProps } = usePress(disabled)
+  const { isHover, hoverProps } = useHover(disabled)
+
+  const buttonState: ButtonState = useMemo(
+    () => ({
+      active: isPressed,
+      hover: isHover,
+      disabled: disabled,
+      autofocus: autofocus,
+    }),
+    [isPressed, isHover, disabled],
+  )
+
+  const allprops = {
+    ...props,
+    ...hoverProps,
+    ...pressProps,
+    ...focusProps,
+    type: type ?? 'button',
+    'data-active': isPressed ? true : undefined,
+    'data-hover': isHover ? true : undefined,
+    'data-disabled': disabled ? true : undefined,
+    'data-autofocus': isFocused ? true : undefined,
+    'data-uilib-state':
+      `${isPressed ? 'active' : ''} ${isHover ? 'hover' : ''} ${isFocused ? 'focus' : ''} `.trim(),
+  }
+
+  const Component = as ?? 'button'
+
+  return (
+    <Component {...allprops}>
+      {typeof children === 'function' ? children(buttonState) : children}
+    </Component>
+  )
 }
